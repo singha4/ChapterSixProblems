d5741141393e4a1c8bfb60b27d0325154971b054
singha4 <amandeep159632478@gmail.com>
2014-05-06 10:09:40 -0400

    version 1

---
+++ Numbers.cs/InchesToFeet.cs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToFeet.cs
+{
+    class Program
+    {
+        public static void Inches(int a)
+        {
+            int feet = a / 12;
+            int inch = a % 12;
+            Console.WriteLine("{0} feet {1} inches", feet, inch);
+        }
+        static void Main(string[] args)
+        {
+            int inches = 67;
+            Inches(inches);
+        }
+    }
+}
