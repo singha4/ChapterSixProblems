f2e18683b6fee918aeb8058f2f0e2632bbb05efe
singha4 <amandeep159632478@gmail.com>
2014-05-07 10:07:52 -0400

    version 2

---
+++ Numbers.cs/InchesToYards.cs/InchesToYards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InchesToYards.cs
+{
+    class InchesToYards
+    {
+        public static void Yards(int a)
+        {
+
+            int feet = a / 12;
+            int yards = feet / 3;
+            feet = feet % 3;
+            int inch = a % 12;
+            Console.WriteLine("{0} yards {1} feet {2} inches", yards, feet, inch);
+        }
+        static void Main(string[] args)
+        {
+            int inches = 67;
+            Yards(inches);
+        }
+    }
+}
