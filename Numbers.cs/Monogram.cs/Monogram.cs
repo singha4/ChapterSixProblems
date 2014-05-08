fa8a9027b2663ebd28b9ad7230df60be22c655a3
singha4 <amandeep159632478@gmail.com>
2014-05-08 10:09:50 -0400

    version 3

---
+++ Numbers.cs/Monogram.cs/Monogram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Monogram.cs
+{
+    class Monogram
+    {
+        public static string DisplayMonogram(string a)
+        {
+            Console.Write("**");
+
+
+            Console.Write("**");
+        }
+        static void Main(string[] args)
+        {
+            string myF = "A", myM = "B", myL = "S", friendF = "K", friendM = "A", friendL = "E";
+            DisplayMonogram(myF + myM + myL);
+            DisplayMonogram(friendF + friendM + friendL);
+        }
+    }
+}
