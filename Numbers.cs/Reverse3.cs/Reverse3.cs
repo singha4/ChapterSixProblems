8ef6aa4ff8f58d95500392c5d75b544cc0008c7b
singha4 <amandeep159632478@gmail.com>
2014-05-12 10:09:27 -0400

    version 7

---
+++ Numbers.cs/Reverse3.cs/Reverse3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverse3.cs
+{
+    class Reverse3
+    {
+        public static void Reverse(int a, int b, int c)
+        {
+            Console.WriteLine("Reversed: {0}, {1}, {2}", a, b, c);
+        }
+        static void Main(string[] args)
+        {
+            int firstInt = 10, middleInt = 20, lastInt = 30;
+            Console.WriteLine("In Order: {0}, {1}, {2}", firstInt, middleInt, lastInt);
+            Reverse(lastInt, middleInt, firstInt);
+
+        }
+    }
+}
---
+++ Numbers.cs/Reverse4.cs/Reverse4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reverse4.cs
+{
+    class Reverse4
+    {
+        public static void Reverse(int a, int b, int c, int d)
+        {
+            Console.WriteLine("Reversed: {0}, {1}, {2}, {3}", a, b, c, d);
+        }
+        static void Main(string[] args)
+        {
+            int firstInt = 10, secondInt = 20, thirdInt = 30, fourthInt = 40;
+            Console.WriteLine("In Order: {0}, {1}, {2} {3}", firstInt, secondInt, thirdInt, fourthInt);
+            Reverse(fourthInt, thirdInt, secondInt, firstInt);
+
+        }
+    }
+}
